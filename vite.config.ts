692d083c3b4968b72dafc46432bab687c5603e65
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2025-02-04 22:20:09 -0600

    Añade el plugin de Codecov a la configuración de Vite y actualiza las dependencias en package.json.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite'
+import { loadEnv, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { codecovVitePlugin } from "@codecov/vite-plugin";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    codecovVitePlugin({
+      enableBundleAnalysis: import.meta.env.VITE_CODECOV_TOKEN !== undefined,
+      bundleName: "testing-with-react",
+      uploadToken: import.meta.env.VITE_CODECOV_TOKEN,
+    }),
+  ],
   
   test: {
     global: true,
     environment: 'jsdom',
     setupFiles: './setupTest.ts'
   },
+
+  
 })
