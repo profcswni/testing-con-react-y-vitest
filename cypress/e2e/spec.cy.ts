7ab23c6eaa08eb78bb8ae759b3fc1f6ad79a8dd6
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2025-02-04 20:01:35 -0600

    Actualiza la configuración de Cypress y añade pruebas para verificar la carga de la aplicación

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,37 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:5173/')
+describe('Render aplication', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('Load the application', () => {
+    cy.visit('/')
+    // Find some element
+    cy.get('h1').should('be.visible')
+
+  })
+
+  // Fint the word Vite
+  it('Find the word Vite', () => {
+    cy.get('h1').contains('Vite + React')
+  })
+
+  // Find the button with the text count is 0
+  it('Find the button with the text count is 0', () => {
+    cy.get('button').contains('count is 0')
+  })
+
+  // Find "Click on the Vite and React logos to learn more"
+  it('Find "Click on the Vite and React logos to learn more"', () => {
+    cy.get('p').contains('Click on the Vite and React logos to learn more')
+  })
+
+  // Fint the logo of Vite
+  it('Find the logo of Vite', () => {
+    cy.get('img').first().should('have.attr', 'alt', 'Vite logo')
+  })
+
+  // Fint the logo of React
+  it('Find the logo of React', () => {
+    cy.get('img').eq(1).should('have.attr', 'alt', 'React logo')
   })
 })
